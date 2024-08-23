fda12af1dfbf3ee5000776ed81688d45fc880d4d
aline necchi <alinenecchi@gmail.com>
2024-08-23 01:40:37 -0300

    adjustments

--- frontend/components/molecules/card-list/usePagination.js
+++ frontend/components/molecules/card-list/usePagination.js
@@ -1,14 +1,15 @@
-import { useMemo } from 'react';
+import { useMemo } from "react";
 
-const DOTS = '...';
+export const DOTS = "...";
 
-const range = (start, end) => Array.from({ length: end - start + 1 }, (_, idx) => idx + start);
+export const range = (start, end) =>
+  Array.from({ length: end - start + 1 }, (_, idx) => idx + start);
 
 export const usePagination = ({
   totalCount,
   pageSize,
   siblingCount = 1,
-  currentPage
+  currentPage,
 }) => {
   const paginationRange = useMemo(() => {
     const totalPageCount = Math.ceil(totalCount / pageSize);
@@ -19,7 +20,10 @@ export const usePagination = ({
     }
 
     const leftSiblingIndex = Math.max(currentPage - siblingCount, 1);
-    const rightSiblingIndex = Math.min(currentPage + siblingCount, totalPageCount);
+    const rightSiblingIndex = Math.min(
+      currentPage + siblingCount,
+      totalPageCount
+    );
 
     const shouldShowLeftDots = leftSiblingIndex > 2;
     const shouldShowRightDots = rightSiblingIndex < totalPageCount - 2;
@@ -35,7 +39,10 @@ export const usePagination = ({
 
     if (shouldShowLeftDots && !shouldShowRightDots) {
       let rightItemCount = 3 + 2 * siblingCount;
-      let rightRange = range(totalPageCount - rightItemCount + 1, totalPageCount);
+      let rightRange = range(
+        totalPageCount - rightItemCount + 1,
+        totalPageCount
+      );
       return [firstPageIndex, DOTS, ...rightRange];
     }
 
