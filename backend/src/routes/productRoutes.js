50081a3495a9cbfa47cc96fe23515d36042b4198
aline necchi <alinenecchi@gmail.com>
2024-08-23 13:37:57 -0300

    add post

--- backend/src/routes/productRoutes.js
+++ backend/src/routes/productRoutes.js
@@ -38,4 +38,31 @@ router.get("/:id", async (req, res) => {
   }
 });
 
+router.post("/", async (req, res) => {
+  const { id, image, name, categories, price, brand } = req.body;
+
+  try {
+    // Create a new product instance
+    const newProduct = new Product({
+      id,
+      image,
+      name,
+      categories,
+      price,
+      brand,
+    });
+
+    // Save the product to the database
+    const savedProduct = await newProduct.save();
+
+    // Respond with the saved product
+    res.status(201).json(savedProduct);
+  } catch (error) {
+    // Handle errors
+    res
+      .status(500)
+      .json({ message: "Error adding product", error: error.message });
+  }
+});
+
 module.exports = router;
